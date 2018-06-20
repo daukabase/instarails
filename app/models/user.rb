9f5c0c8935c2ca5442a0b65eb53d3ba69a3f0875
daukabase <daukabase@gmail.com>
2018-06-20 14:00:31 +0600

    add rememberme with cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	attr_accessor :remember_token
 	has_secure_password
 	has_one_attached :avatar
 	has_many :posts
@@ -13,5 +14,33 @@ class User < ApplicationRecord
 
 	before_create {self.email = email.downcase}
 	before_create {self.username = username.downcase}
+	class << self
+		def digest(string)
+	    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+	                                                  BCrypt::Engine.cost
+	    BCrypt::Password.create(string, cost: cost)
+	  end
 
+	  # Returns a random token.
+	  def new_token
+	    SecureRandom.urlsafe_base64
+	  end
+	end
+
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
+
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+	def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
